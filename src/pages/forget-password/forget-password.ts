243e3346a70e6397fe8c14d1b9c7585a4c2d58d7
Pratishtha Pandey <pratpandey@deloitte.com>
2018-05-12 08:22:36 +0530

    added Toast message for forget password scenario

--- src/pages/forget-password/forget-password.ts
+++ src/pages/forget-password/forget-password.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { LoginPage} from '../login/login';
+import { ToastController } from 'ionic-angular';
 /**
  * Generated class for the ForgetPasswordPage page.
  *
@@ -15,13 +16,22 @@ import { LoginPage} from '../login/login';
 })
 export class ForgetPasswordPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ForgetPasswordPage');
   }
+  presentToast(position: string) {
+    let toast = this.toastCtrl.create({
+      message: 'Mail sent to your registerd email-id to RESET the password.',
+      duration: 3000,
+      position: position
+    });
+    toast.present();
+  }
   navigateToLogin(){
+    this.presentToast('bottom');
     this.navCtrl.push(LoginPage);
   }
 
