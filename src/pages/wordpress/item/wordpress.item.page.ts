4bc0fd7f30efc2df9acbb6124f104960375cfae0
Pratishtha Pandey <pratpandey@deloitte.com>
2018-05-12 07:39:33 +0530

    Answer question

--- src/pages/wordpress/item/wordpress.item.page.ts
+++ src/pages/wordpress/item/wordpress.item.page.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { AskPage } from '../../../pages/ask/ask';
 
 @Component({
 	templateUrl: 'wordpress.item.html'
 })
 export class WordpressItemPage {
-	
+	private nav: NavController;
 	post = {};
 
-	constructor(navParams: NavParams) {
+	constructor(navParams: NavParams,nav: NavController) {
 		this.post = navParams.get('item');
-		console.log("item",this.post);
+		this.nav = nav;
+	}
+	answerQuestion(){
+		this.nav.push(AskPage, {
+			isAnswer: true
+		});
 	}
 }
